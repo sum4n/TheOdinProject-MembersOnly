229309884898865722d7b93e64d476fbb0cf33cc
Suman Das <sum4ndas@gmail.com>
2024-06-22 06:27:45 +0530

    Define user schema and message schema

---
+++ models/message.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  title: { type: String, required: true, maxLength: 200 },
+  text: { type: String, required: true, maxLength: 1000 },
+  timestamp: { type: Data, default: Date.now() },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+// Export model
+module.exports = mongoose.model("Message", MessageSchema);
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  last_name: { type: String, required: true, maxLength: 100 },
+  email: { type: email, required: true, maxLength: 100 },
+  password: { type: String, required: true },
+  membership_status: { type: Boolean, default: false },
+});
+
+// Virtual for user's full name.
+UserSchema.virtual("name").get(function () {
+  let fullname = "";
+  if (this.first_name && this.last_name) {
+    fullname = `${this.first_name} ${this.last_name}`;
+  }
+  return fullname;
+});
+
+// Export model
+module.exports = mongoose.model("User", UserSchema);
