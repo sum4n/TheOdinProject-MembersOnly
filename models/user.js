954ce50d926d2c922b9a15227d5d4d1c2bcda9d9
Suman Das <sum4ndas@gmail.com>
2024-06-22 07:21:48 +0530

    Change email type to String, make email unique

--- models/user.js
+++ models/user.js
@@ -5,7 +5,12 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
-  email: { type: email, required: true, maxLength: 100 },
+  email: {
+    type: String,
+    maxLength: 100,
+    required: true,
+    unique: true,
+  },
   password: { type: String, required: true },
   membership_status: { type: Boolean, default: false },
 });
