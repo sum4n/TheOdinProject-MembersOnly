e3ca0face88921f61c68a2ff89ef499c52dec457
Suman Das <sum4ndas@gmail.com>
2024-06-23 07:28:03 +0530

    Implement signUp_form GET

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,9 @@ const User = require("../models/user");
 const asyncHandler = require("express-async-handler");
 
 // Display User create form on GET
-exports.user_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Not Implemented: User create GET");
-});
+exports.user_create_get = (req, res, next) => {
+  res.render("signUp_form", { title: "Sign Up" });
+};
 
 // Handle User create on POST
 exports.user_create_post = asyncHandler(async (req, res, next) => {
