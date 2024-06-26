4adc08fe16c45a1998379a95310c0d29e9a5af86
Suman Das <sum4ndas@gmail.com>
2024-06-26 09:04:20 +0530

    Add membership-status get/post routes

--- routes/routes.js
+++ routes/routes.js
@@ -6,9 +6,18 @@ const user_controller = require("../controllers/userController");
 // User routes
 
 // Get request for creating a user.
-router.get("/sign-up", user_controller.user_create_get);
+router.get("/user/sign-up", user_controller.user_create_get);
 
 // Post request for creating a user.
-router.post("/sign-up", user_controller.user_create_post);
+router.post("/user/sign-up", user_controller.user_create_post);
+
+// Get request for membership
+router.get("/user/:id/membership-status", user_controller.user_membership_get);
+
+// Post request for membership
+router.post(
+  "/user/:id/membership-status",
+  user_controller.user_membership_post
+);
 
 module.exports = router;
