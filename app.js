cbdef2668f087b3529ab9f8a0f576e7d98537529
Suman Das <sum4ndas@gmail.com>
2024-06-30 07:51:59 +0530

    Implement authentication using PassportJS

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const bcrypt = require("bcryptjs");
+
+const User = require("./models/user");
 
 // Import routes
 const router = require("./routes/routes");
@@ -10,6 +17,10 @@ require("dotenv").config();
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+// Session and passport
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.session());
 
 // Required to populate req.body with form fields.
 app.use(express.json());
@@ -25,10 +36,62 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+// Authentication
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect Password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+// Home page route
 app.get("/", (req, res) => {
   res.render("index", { user: req.user });
 });
 
+// Other routes
 app.use("/", router);
+
+// Login, logout routes
+app.post(
+  "/user/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
+app.get("/user/log-out", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
