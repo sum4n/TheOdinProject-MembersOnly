c7de0a0fbfaf84d2aec2f39faf5777caa31e38be
Suman Das <sum4ndas@gmail.com>
2024-07-03 09:59:32 +0530

    Add port variable

--- app.js
+++ app.js
@@ -107,4 +107,6 @@ app.get("/user/log-out", (req, res, next) => {
   });
 });
 
-app.listen(3000, () => console.log("app listening on port 3000!"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("app listening on port 3000!"));
