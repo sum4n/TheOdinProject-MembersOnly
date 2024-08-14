70967c4c79f742a7008ded3aad5f7324cc152735
Suman Das <sum4ndas@gmail.com>
2024-08-14 22:20:07 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+
+const passport = require("./config/passport");
+const session = require("./config/session");
+
+const app = express();
+
+// Set views path and view engine.
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Use session and passport middleware.
+app.use(session);
+app.use(passport.session());
+
+// Middleware to parse request body.
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Serve static assests.
+app.use(express.static(path.join(__dirname, "public")));
+
+// Import and use routes.
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const messagesRouter = require("./routes/messages");
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/messages", messagesRouter);
+
+// Start server.
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));
