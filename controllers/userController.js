8fd97537c83cae5fc9cdc29e2a74f6a6262cc148
Suman Das <sum4ndas@gmail.com>
2024-08-15 12:26:32 +0530

    Add sign-up form validation and update sign_up_post controllers to send validation errors

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,6 +5,44 @@ const passport = require("passport");
 const pool = require("../db/pool");
 const bcrypt = require("bcryptjs");
 
+const validateSignUpForm = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified."),
+  body("last_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Last name must be specified."),
+  body("username")
+    .trim()
+    .isLength({ min: 3 })
+    .escape()
+    .withMessage("Username must be at least 3 characters long.")
+    .isAlphanumeric()
+    .withMessage("Username must contain only letters and numbers")
+    .custom(async (username) => {
+      const res = await pool.query(
+        "SELECT user_id FROM users WHERE username = $1",
+        [username]
+      );
+      if (res.rows.length > 0) {
+        return Promise.reject("Username already in use.");
+      }
+    }),
+  body("password")
+    .trim()
+    .isLength({ min: 4 })
+    .withMessage("Password must be at least 4 characters long."),
+  body("confirm_password")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Passwords do not match"),
+];
+
 exports.log_in_get = asyncHandler(async (req, res) => {
   res.render("pages/log-in");
 });
@@ -13,10 +51,35 @@ exports.log_in_post = asyncHandler(async (req, res) => {
   res.send("POST log-in: WIP");
 });
 
-exports.register_get = asyncHandler(async (req, res) => {
-  res.render("pages/register");
+exports.sign_up_get = asyncHandler(async (req, res) => {
+  res.render("pages/sign-up");
 });
 
-exports.register_post = asyncHandler(async (req, res) => {
-  res.send("POST register: WIP");
-});
+exports.sign_up_post = [
+  validateSignUpForm,
+  asyncHandler(async (req, res) => {
+    // Extract validation errors from a request.
+    const errors = validationResult(req);
+
+    const data = {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      password: req.body.password,
+      confirm_password: req.body.confirm_password,
+    };
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/errors messages.
+      // console.log(errors.array());
+      res.render("pages/sign-up", {
+        title: "Sign-Up",
+        data: data,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      res.send(data);
+    }
+  }),
+];
