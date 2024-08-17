424f342ac9b8dc541c556e4b13500c5fcf8da46d
Suman Das <sum4ndas@gmail.com>
2024-08-17 08:25:50 +0530

    Add dashboard and log-out routes

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,8 @@ router.post("/log-in", userController.log_in_post);
 router.get("/sign-up", userController.sign_up_get);
 router.post("/sign-up", userController.sign_up_post);
 
+router.get("/dashboard", userController.dashboard_get);
+
+router.get("/log-out", userController.log_out);
+
 module.exports = router;
