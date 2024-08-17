bcd2441e90feedd2ba270544c4719393904acdcf
Suman Das <sum4ndas@gmail.com>
2024-08-17 09:02:39 +0530

    Remove dashboard route from users routes and it to index route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const { isAuth } = require("../middlewares/authMiddleware");
+const userController = require("../controllers/userController");
+
 router.get("/", (req, res) => {
-  res.redirect("/users/log-in");
+  res.redirect("/dashboard");
 });
+
+router.get("/dashboard", isAuth, userController.dashboard_get);
 
 module.exports = router;
