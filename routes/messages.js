cd5bbcf0844452f71c86dac140eb1e344aad323c
Suman Das <sum4ndas@gmail.com>
2024-08-19 08:22:29 +0530

    Update messages route to add message create get link and controller

--- routes/messages.js
+++ routes/messages.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const messageController = require("../controllers/messageController");
+
+router.get("/create", messageController.message_create_get);
+
 module.exports = router;
