848a7d87b33ce6a50812a6f4d77d7089bd47826a
Suman Das <sum4ndas@gmail.com>
2024-08-21 08:24:47 +0530

    Add authentication middleware for messages/create get and post routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const { isAuth } = require("../middlewares/authMiddleware");
 const messageController = require("../controllers/messageController");
 
-router.get("/create", messageController.message_create_get);
+router.get("/", messageController.allMessages_get);
 
-router.post("/create", messageController.message_create_post);
+router.get("/create", isAuth, messageController.message_create_get);
+
+router.post("/create", isAuth, messageController.message_create_post);
 
 module.exports = router;
