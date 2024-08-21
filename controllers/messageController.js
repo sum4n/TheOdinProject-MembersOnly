8c0fb29562ffc618ddee901fec2def1ec1c76f65
Suman Das <sum4ndas@gmail.com>
2024-08-21 08:27:00 +0530

    Add allMessage_get controller to get messages depending on user status

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,6 +3,23 @@ const { body, validationResult } = require("express-validator");
 
 const pool = require("../db/pool");
 
+module.exports.allMessages_get = asyncHandler(async (req, res) => {
+  let allMessages;
+
+  if (req.user && req.user.membership) {
+    allMessages = await pool.query(
+      "SELECT title, content, created_at, updated_at, first_name, last_name, messages.message_id, users.user_id FROM messages JOIN users ON messages.user_id = users.user_id"
+    );
+  } else {
+    allMessages = await pool.query("SELECT title, content FROM messages");
+  }
+
+  res.render("pages/messages", {
+    title: "Messages",
+    allMessages: allMessages.rows,
+  });
+});
+
 module.exports.message_create_get = (req, res) => {
   res.render("pages/createMessage", {
     title: "Create Message",
@@ -10,13 +27,9 @@ module.exports.message_create_get = (req, res) => {
 };
 
 module.exports.message_create_post = asyncHandler(async (req, res) => {
-  if (req.user) {
-    await pool.query(
-      "INSERT INTO messages (title, content, user_id) VALUES ($1, $2, $3)",
-      [req.body.msgTitle, req.body.msgContent, req.user.user_id]
-    );
-    res.redirect("/messages");
-  } else {
-    res.redirect("/users/log-in");
-  }
+  await pool.query(
+    "INSERT INTO messages (title, content, user_id) VALUES ($1, $2, $3)",
+    [req.body.msgTitle, req.body.msgContent, req.user.user_id]
+  );
+  res.redirect("/messages");
 });
