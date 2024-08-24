148075506d2279b3ecec87536a8ff893fc968160
Suman Das <sum4ndas@gmail.com>
2024-08-24 20:47:22 +0530

    Add /dashboard/membership post and /dashboard/admin post to handle membership and admin toggle

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,11 @@ router.get("/", (req, res) => {
 });
 
 router.get("/dashboard", isAuth, userController.dashboard_get);
-router.post("/dashboard", isAuth, userController.dashboard_post);
+router.post(
+  "/dashboard/membership",
+  isAuth,
+  userController.dashboard_membership_post
+);
+router.post("/dashboard/admin", isAuth, userController.dashboard_admin_post);
 
 module.exports = router;
