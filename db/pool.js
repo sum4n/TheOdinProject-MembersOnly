82ef026ca2b86214feaea45024b65c9df7f45294
Suman Das <sum4ndas@gmail.com>
2024-08-26 14:52:42 +0530

    Update db/pool.js to use neon database service

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,28 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+PGPASSWORD = decodeURIComponent(PGPASSWORD);
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: "require",
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
 });
+
+// const pool = new Pool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 
 module.exports = pool;
