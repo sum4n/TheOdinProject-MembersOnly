38310bf1b0989bdb54d589509a4af28c425764dd
Suman Das <sum4ndas@gmail.com>
2024-08-26 12:05:13 +0530

    Replace all database queries with db functions from db/queries.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,22 +1,25 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
-const pool = require("../db/pool");
+const {
+  getAllMessagesWithUser,
+  getAllMessages,
+  createMessage,
+  deleteMessage,
+} = require("../db/queries");
 
 module.exports.allMessages_get = asyncHandler(async (req, res) => {
   let allMessages;
 
   if ((req.user && req.user.membership) || (req.user && req.user.admin)) {
-    allMessages = await pool.query(
-      "SELECT title, content, created_at, updated_at, first_name, last_name, messages.message_id, users.user_id FROM messages JOIN users ON messages.user_id = users.user_id"
-    );
+    allMessages = await getAllMessagesWithUser();
   } else {
-    allMessages = await pool.query("SELECT title, content FROM messages");
+    allMessages = await getAllMessages();
   }
 
   res.render("pages/messages", {
     title: "Messages",
-    allMessages: allMessages.rows,
+    allMessages: allMessages,
   });
 });
 
@@ -27,17 +30,13 @@ module.exports.message_create_get = (req, res) => {
 };
 
 module.exports.message_create_post = asyncHandler(async (req, res) => {
-  await pool.query(
-    "INSERT INTO messages (title, content, user_id) VALUES ($1, $2, $3)",
-    [req.body.msgTitle, req.body.msgContent, req.user.user_id]
-  );
+  await createMessage(req.body.msgTitle, req.body.msgContent, req.user.user_id);
+
   res.redirect("/messages");
 });
 
 module.exports.message_delete_post = asyncHandler(async (req, res) => {
-  await pool.query("DELETE FROM messages WHERE message_id = $1", [
-    req.params.message_id,
-  ]);
+  await deleteMessage(req.params.message_id);
 
   res.redirect("back");
 });
