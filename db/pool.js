904e99fc03e7d146fad94a64d7a5e2a138fc313f
Suman Das <sum4ndas@gmail.com>
2024-08-26 15:25:06 +0530

    Update db/pool.js to use neon connection string

--- db/pool.js
+++ db/pool.js
@@ -2,27 +2,8 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-PGPASSWORD = decodeURIComponent(PGPASSWORD);
-
 const pool = new Pool({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-  ssl: "require",
-  connection: {
-    options: `project=${ENDPOINT_ID}`,
-  },
+  connectionString: process.env.DATABASE_URL,
 });
-
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
 
 module.exports = pool;
